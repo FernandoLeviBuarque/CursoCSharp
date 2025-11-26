f0c741b10d65b7b6a9ca8485c8f89d1c65cf4ed0
FernandoLeviBuarque <fernandolevibuarque@gmail.com>
2025-11-26 20:01:40 -0300

    Add files via upload

---
+++ Quesstao07Lista2.cs
@@ -0,0 +1,39 @@
+﻿/*  Crie um programa que leia o nome de um funcionário, seu salário, quantos anos
+ ele trabalha na empresa e mostre seu novo salário, reajustado de acordo com a tabela 
+a seguir: 
+- Até 3 anos de empresa: 3% de aumento
+- Entre 3 (inclusive) e menos de 10 anos: Aumento de 12.5%
+- 10 Anos ou mais: aumento de 20% */
+
+using System;
+
+class CalculodoSalario
+{
+    static void Main()
+    {   // Entrada de dados dos funcionários
+        Console.Write("Digite o nome do funcionário: ");
+        string nome = Console.ReadLine();
+        Console.Write("Digite o salário do funcionário: ");
+        double salario = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite quantos anos ele trabalha na empresa: ");
+        int anosEmp = Convert.ToInt32(Console.ReadLine());
+
+        // Calculo do novo salario a partir do tempo de empresa
+        if (anosEmp < 3)
+        {
+            salario *= 1.03;
+        }
+        else if (anosEmp >= 3 && anosEmp < 10)
+        {
+            salario *= 1.125;
+        }
+        else
+        {
+            salario *= 1.20;
+        }
+        // Exibição do novo salário
+        Console.WriteLine($"O novo salário do funcionário {nome} é: R$ {salario:F2}"); // Formatação para 2 casas decimais (Copilot)
+
+        Console.WriteLine(nome + " possui " + anosEmp + " anos de empresa.");
+    }
+}
---
+++ Questão08Lista2.cs
@@ -0,0 +1,32 @@
+﻿/* Crie um programa para aprovar ou não o empréstimo bancário para a compra de uma casa.
+ O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai
+pagar. Calcule o valor da prestação mensal, sabendo ue ela não pode exceder 30% do salário
+ou então o empréstimo será negado.*/
+
+class AprovaCredito 
+{
+    static void Main(string[] args)
+    {
+        // Entrada de dados
+        Console.WriteLine("Digite o valor do imovel desejado: ");
+        double valorCasa = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o seu salário: ");
+        double salarioComprador = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Qual o prazo para pagamento: ");
+        int anosParaPagar = Convert.ToInt32(Console.ReadLine());
+        double prestacaoMensal = valorCasa / (anosParaPagar * 12);
+        double limitePrestacao = salarioComprador * 0.3;
+        
+        // Calculo do limite permitido
+        if (prestacaoMensal <= limitePrestacao)
+        {
+            Console.WriteLine("Empréstimo concedido.");
+            Console.WriteLine($"Sua prestação mensal será de: {prestacaoMensal:C2}");// Fomato do copilot
+        }
+        else
+        {
+            Console.WriteLine("Empréstimo negado.");
+            Console.WriteLine($"Infelizmente, a sua prestação mensal de {prestacaoMensal:C2} excede 30% da sua renda.");
+        }
+    }
+}
